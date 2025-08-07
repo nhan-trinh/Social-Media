17efd1b853c6582ddf3181e94594e853ad05dec1
nhantrinh <nhantrinh989@gmail.com>
2025-08-07 14:40:33 +0700

    Update imagekit.js

--- server/configs/imagekit.js
+++ server/configs/imagekit.js
@@ -5,7 +5,7 @@ import ImageKit from "imagekit";
 var imagekit = new ImageKit({
     publicKey : process.env.IMAGEKIT_PUBLIC_KEY,
     privateKey : process.env.IMAGEKIT_PRIVATE_KEY,
-    urlEndpoint : process.env.IMAHEKIT_URL_ENDPOINT
+    urlEndpoint : process.env.IMAGEKIT_URL_ENDPOINT
 });
 
 export default imagekit
