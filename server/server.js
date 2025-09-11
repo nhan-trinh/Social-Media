1d9cae57d80b4ed25586a6efb62bd148e289ae6f
nhantrinh <nhantrinh989@gmail.com>
2025-09-11 23:01:50 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 import "dotenv/config";
 import connectDB from "./configs/db.js";
 import { inngest, functions } from "./inngest/index.js";
@@ -19,6 +21,22 @@ await connectDB();
 app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware());
+
+// Static cache headers for client build (if served here in prod)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const clientDist = path.join(__dirname, "../client/dist");
+app.use(
+  express.static(clientDist, {
+    maxAge: "365d",
+    immutable: true,
+    setHeaders: (res, filePath) => {
+      if (/\.(html)$/.test(filePath)) {
+        res.setHeader("Cache-Control", "no-cache");
+      }
+    },
+  })
+);
 
 app.get("/", (req, res) => res.send("Server is running"));
 app.use("/api/inngest", serve({ client: inngest, functions }));
